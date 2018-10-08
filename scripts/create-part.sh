f76762790c7c224a50e3b94bcab4a6644118aa94
Jesus Almaraz <almarazj@ie.ibm.com>
2018-10-08 12:46:55 +0200

    add nfs-common when rhel

--- scripts/create-part.sh
+++ scripts/create-part.sh
@@ -45,6 +45,11 @@ sudo partprobe
 sudo mkfs.ext4 ${device}1
 echo "${device}1 ${path}   ext4  defaults   0 0" | sudo tee -a /etc/fstab
 
-sudo apt -y install nfs-common
+if [[ "${OSLEVEL}" == "ubuntu" ]]
+then
+  sudo apt -y install nfs-common
+else
+  sudo yum -y install nfs-utils
+fi
 
 sudo mount -a
